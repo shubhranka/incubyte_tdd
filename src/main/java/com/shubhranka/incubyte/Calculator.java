db8beec7ff0c4867f6065886144362a2450a1168
shubhrankavarma@gmail.com <shubhrankavarma@gmail.com>
2024-12-17 20:28:19 +0530

    tested for numbers with custom seperator

--- src/main/java/com/shubhranka/incubyte/Calculator.java
+++ src/main/java/com/shubhranka/incubyte/Calculator.java
@@ -1,13 +1,34 @@
 package com.shubhranka.incubyte;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
+
+        String[] numbersArray;
         
-        String[] numbersArray = splitNumbersByCommaOrNewLine(numbers);
+        if (numbers.startsWith("//")) {
+            numbersArray = splitByCustomDelimiter(numbers);
+        }else{
+            numbersArray = splitNumbersByCommaOrNewLine(numbers);
+        }
         return sum(numbersArray);
+    }
+
+    private String[] splitByCustomDelimiter(String numbers) {
+        String delimiterRegex = "//(.)\n(.*)";
+        Pattern pattern = Pattern.compile(delimiterRegex);
+        Matcher matcher = pattern.matcher(numbers);
+        if (matcher.find()) {
+            String delimiter = matcher.group(1);
+            String numbersWithoutDelimiter = matcher.group(2);
+            return numbersWithoutDelimiter.split(delimiter);
+        }
+        return new String[0];
     }
 
     private String[] splitNumbersByCommaOrNewLine(String numbers) {
--- src/test/java/com/shubhranka/incubyte/CalculatorTest.java
+++ src/test/java/com/shubhranka/incubyte/CalculatorTest.java
@@ -31,5 +31,14 @@ public class CalculatorTest {
         assertEquals(6, calculator.add("1\n2\n3"));
         assertEquals(10, calculator.add("1,2\n3,4"));
         assertEquals(15, calculator.add("1\n2,3\n4,5"));
+        assertEquals(1, calculator.add("1\n"));  
+        assertEquals(1, calculator.add("1,"));
+    }
+
+    @Test
+    public void shouldReturnSumIfCustomDelimiterIsProvided() {
+        assertEquals(3, calculator.add("//;\n1;2"));
+        assertEquals(6, calculator.add("//:\n1:2:3"));
+        assertEquals(10, calculator.add("//t\n1t2t3t4"));
     }
 }
