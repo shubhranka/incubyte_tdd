ddcac2700641e64f7c2e0fbc65d8d383b838527e
shubhrankavarma@gmail.com <shubhrankavarma@gmail.com>
2024-12-17 21:56:14 +0530

    completed

--- src/main/java/com/shubhranka/incubyte/Main.java
+++
@@ -1,9 +0,0 @@
-package com.shubhranka.incubyte;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.add("//[*][%]\n1*2%3"));
-    }
-}
